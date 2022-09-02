cfadd154aafc2ae981d4a84fd20a5dd54bf7e0a4
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2022-09-02 16:50:16 +0100

    Added coinbase and web3 connect

--- src/hooks/connect.ts
+++ src/hooks/connect.ts
@@ -1,24 +1,69 @@
 import { useState } from "react";
+import { ethers, providers } from "ethers";
+import Web3Modal from "web3modal";
+import WalletConnect from "@walletconnect/web3-provider";
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+import { providerOptions } from "../libs/config";
+import { toHex } from "../libs/utils";
 
-export const useConnectWallet = async () => {
-
-    const [_provider, setProvider] = useState(null)
-    const [_liberay, setLibrary] = useState(null)
-    const [chainId, setChainId] = useState(null)
-    const [accounts, setAccount] = useState(null)
 
 
-    
-    try {
-      const provider = await web3Modal.connect();
-      const library = new ethers.providers.Web3Provider(provider);
-      const accounts = await library.listAccounts();
-      const network = await library.getNetwork();
-      setProvider(provider);
-      setLibrary(library);
-      if (accounts) setAccount(accounts[0]);
-      setChainId(network.chainId);
-    } catch (error) {
-      setError(error);
+export const useConnectWallet = () => {
+
+    const [provider, setProvider] = useState<any>(null)
+    const [library, setLibrary] = useState<any>(null)
+    const [chainId, setChainId] = useState(1)
+    const [accounts, setAccount] = useState<any>(null)
+    const [error, setError] = useState<any>(null)
+
+    const connect = async () => {
+
+        try {
+
+            const web3Modal = new Web3Modal({
+                cacheProvider: false, // optional
+                providerOptions // required
+            });
+              
+
+            const provider = await web3Modal.connect();
+            const library = new ethers.providers.Web3Provider(provider);
+            const accounts = await library.listAccounts();
+            const network = await library.getNetwork();
+            setProvider(provider);
+            setLibrary(library);
+            if (accounts) setAccount(accounts[0]);
+            setChainId(network.chainId);
+        } catch (error) {
+            setError(error);
+        }
+
     }
+
+    const getAccount = async () => {
+
+        try {
+
+            const web3Modal = new Web3Modal({
+                cacheProvider: false, // optional
+                providerOptions // required
+            });
+              
+            const provider = await web3Modal.connect();
+            const library = new ethers.providers.Web3Provider(provider);
+            const accounts = await library.listAccounts();
+            const network = await library.getNetwork();
+            setProvider(provider);
+            setLibrary(library);
+            if (accounts) setAccount(accounts[0]);
+            setChainId(network.chainId);
+
+        } catch (error) {
+            setError(error);
+        }
+
+    }
+    
+    return { connect, getAccount, accounts, chainId, library, error, provider }
+
 };
--- src/libs/functions.ts
+++ src/libs/functions.ts
@@ -2,87 +2,18 @@ import { ethers, providers } from "ethers";
 import Web3Modal from "web3modal";
 import tokenABI from "./abi.json";
 import { providerOptions } from "./config";
-import WalletConnectProvider from "@walletconnect/web3-provider";
 
-
-
-    
-export async function checkConnectedWalletExist() {
-
-    try {
-
-        if (typeof window?.ethereum !== 'undefined') {
-
-            const { ethereum } = window;
-
-            const accounts = await ethereum.request({ method: "eth_accounts" });
-
-            if (accounts.length !== 0) return accounts[0];
-
-            return null;
-
-        } else {
-            const provider = await walletConnect()
-
-            const accounts = await provider.getSigner().getAddress();
-
-            return accounts
-
-        }
-
-    } catch (e) {
-        console.log(e)
-        return null
-    }
-
-}
-
-
-
-export async function connect() {
-
-    try {
-
-        if (typeof window?.ethereum !== 'undefined') {
-
-            const web3Modal = new Web3Modal({
-                cacheProvider: true, // optional
-                providerOptions: providerOptions  as any // required
-            });
-            
-
-            const instance = await web3Modal.connect();
-            const provider = new ethers.providers.Web3Provider(instance);
-            const accounts = await provider.getSigner().getAddress();
-
-        } else {
-
-            const provider = await walletConnect()
-
-            const accounts = await provider.getSigner().getAddress();
-
-            console.log(accounts)
-
-        }
-
-    } catch (error) {
-        console.error(error);
-    }
-    
-}
 
 export async function transfer(tokenAddress: string, to: string, amount: number){
 
     const ABI = tokenABI
 
     const web3Modal = new Web3Modal({
-        // network: "mainnet", // optional
-        //cacheProvider: true, // optional
-        disableInjectedProvider: false,
-        providerOptions: providerOptions as any, // required
-    });
+        cacheProvider: false, // optional
+        providerOptions // required
+      });
+      
 
-    // const instance = await web3Modal.connectTo("walletconnect");
     const instance = await web3Modal.connect();
     const provider = new ethers.providers.Web3Provider(instance);
     await provider.send("eth_requestAccounts", []);
@@ -103,47 +34,30 @@ export async function transfer(tokenAddress: string, to: string, amount: number)
 
 export async function balanceOf(tokenAddress: string, address: string){
 
+    const web3Modal = new Web3Modal({
+        cacheProvider: false, // optional
+        providerOptions // required
+    });
+      
     const ABI = tokenABI
 
-    const web3Modal = new Web3Modal({
-        // network: "mainnet", // optional
-        //cacheProvider: true, // optional
-        disableInjectedProvider: false,
-        providerOptions: providerOptions as any, // required
-    });
-
-    // const instance = await web3Modal.connectTo("walletconnect");
     const instance = await web3Modal.connect();
+
     const provider = new ethers.providers.Web3Provider(instance);
+
     await provider.send("eth_requestAccounts", []);
+
     const signer = provider.getSigner();
+
     const currentContract = new ethers.Contract(
         tokenAddress,
         ABI,
         signer
     );
 
+
     const result = await currentContract.balanceOf(address);
 
     return parseInt(result._hex, 16) / (10 ** 18)
 }
-
-export const walletConnect = async () => {
-
-    //  Create WalletConnect Provider
-    const provider = new WalletConnectProvider({
-        infuraId: process.env.NEXT_PUBLIC_API_KEY,
-    });
-
-    // //  Enable session (triggers QR Code modal)
-    await provider.enable();
-
-
-    const web3Provider = new providers.Web3Provider(provider);
-
-    return web3Provider
-  
-  
-}
-
 
